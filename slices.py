4b95fd44698410eb7991b931dfac0db36d58b2b3
danielditer <30413308+danielditer@users.noreply.github.com>
2023-12-17 05:35:40 +0000

    updated slices.py

--- slices.py
+++ slices.py
@@ -1,9 +1,13 @@
 # Slices
-players = ['charles', 'martina', 'michael', 'florence', 'eli']
+players = ['charles', 'martina', 'michael', 'a','b','florence', 'eli']
 print('The first three elements in the list are:')
 for player in players[:3]:
     print(player.title())
 
 print('The last three elements in the list are:')
 for player in players[-3:]:
+    print(player.title())
+
+print('The items from the middle of the list are:')
+for player in players[int(len(players)/2)+1-2:int(len(players)/2)+2]:
     print(player.title())
