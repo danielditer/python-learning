465b8165ab2c24213243dbd86c74da0f50370bda
danielditer <30413308+danielditer@users.noreply.github.com>
2023-12-25 02:04:15 +0000

    pizzas

--- slices.py
+++ slices.py
@@ -9,8 +9,21 @@ for player in players[-3:]:
     print(player.title())
 
 print('The items from the middle of the list are:')
-for player in players[int(len(players)/2)+1-2:int(len(players)/2)+2]:
+for player in players[int(len(players)/2)-1:int(len(players)/2)+2]:
     print(player.title())
 
 #pizzas
 pizzas = ['Margherita','Pepperoni','Hawaiian','Veggie Supreme','BBQ Chicken', 'Pesto and Tomato']
+
+pizzas_copy = pizzas[:]
+
+pizzas.append('pizza1')
+pizzas_copy.append('pizza2')
+
+print("My favorite pizzas are:")
+for pizza in pizzas:
+    print(pizza)
+
+print("My friend's favorite pizzas are:")
+for friend_pizza in pizzas_copy:
+    print(friend_pizza)
