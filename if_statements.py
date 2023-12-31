5b2d86741b8d4e1c36d942f2d75593c29fff1b1e
danielditer <30413308+danielditer@users.noreply.github.com>
2023-12-31 18:03:07 +0000

    stages of life

--- if_statements.py
+++ if_statements.py
@@ -25,7 +25,22 @@ for color in alien_color:
         print('The player just earned 10 points.\n')
     elif color == 'red':
         print('The player just earned 15 points.\n')
-        
-        
 
-    
+# Stages of life
+
+ages = [3,1, 78, 49, 7] 
+
+for age in ages:
+    print(f"The person's age is {age}")
+    if age < 2:
+        print('The person is a baby.\n')
+    elif age > 1 and age < 4:
+        print('The person is a toddler.\n')
+    elif age > 3 and age < 13:
+        print('The person is a kid.\n')
+    elif age > 12 and age < 20:
+        print('The person is a teenager.\n')
+    elif age > 19 and age < 65:
+        print('The person is an adult.\n')
+    else:
+        print('The person is an elder.\n')    
