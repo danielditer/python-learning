d2f034d3e88c0ab18e1ea4208370b5c1e415095d
danielditer <30413308+danielditer@users.noreply.github.com>
2024-02-28 13:02:27 -0400

    'dictionaries.py'

---
+++ dictionaries.py
@@ -0,0 +1,25 @@
+# 6-1 Person
+
+person = { 
+    'first_name': 'daniel', 
+    'last_name': 'caballero',
+    'age': 36,
+    'city': 'Cochabamba'
+    }
+
+for k, v in person.items():
+    print(f'My {k} is {v}\n')
+
+# 6-2 Favorite Number
+
+favorite_numbers = {
+    'sarah': 45,
+    'ann': 23,
+    'joseph': 324,
+    'phil': 42,
+    'brandon': 12,
+    'john': 7890
+}
+
+for k, v in favorite_numbers.items():
+    print(f'My name is {key} and my favorite number is {value}')
