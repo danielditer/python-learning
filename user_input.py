52d31ce6e590cc06061e4005e4309d7e3546bd46
Daniel Caballero <danielditer@gmail.com>
2024-04-16 23:02:08 -0400

    add user-input

--- user_input.py
+++ user_input.py
@@ -19,4 +19,4 @@ number = int(number)
 if number % 10 == 0:
     print(f"The number {number} is multiple of 10.")
 else:
-    print(f"The number {number} is not multiple of 10."):
+    print(f"The number {number} is not multiple of 10.")
